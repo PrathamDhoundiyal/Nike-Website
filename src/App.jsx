f57b7586c3fd9e47150cd0d071eaf41b5ed04634
Pratham Dhoundiyal <prathamdhoundiyal@gmail.com>
2026-04-12 13:26:24 +0530

    12April_CodeFix

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
-import React from 'react'
- 
+import React from "react";
+
 const App = () => {
   return (
-    <main className='relative'>
-Nav
-<section className=''>
-  Hero
-</section>
+    <main className="relative ">
+      Nav
+      <section className="">Hero</section>
+      <section className="">PopularProducts</section>
+      <section className="">SuperQuality</section>
+      <section className="">Services</section>
+      <section className="">SpecialOffer</section>
     </main>
-  )
-}
+  );
+};
 
-export default App
-
+export default App;
